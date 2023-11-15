31db115ab27e675d84c25f9bc43138138469141e
geroi1 <99471303+geroi1@users.noreply.github.com>
2023-11-15 20:09:13 +0700

    Add files via upload

--- main/models.py
+++ main/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
-class Article(models.Model):
+class Arcticle(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+    author = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True, unique=True)
+    
+class Tags(models.Model):
+    name = models.CharField(max_length=255, db_index=True, unique=True)
